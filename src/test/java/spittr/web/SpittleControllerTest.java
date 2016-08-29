1e8f25df19abb8011e41cfce8ea84bcfb85730af
kimtoto <kimtoto0619@gmail.com>
2016-08-29 20:55:44 +0900

    good shouldshowReg

---
+++ src/test/java/spittr/web/SpittleControllerTest.java
@@ -0,0 +1,83 @@
+package spittr.web;
+
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.servlet.view.InternalResourceView;
+
+import spittr.Spittle;
+import spittr.data.SpittleRepository;
+
+public class SpittleControllerTest {
+
+	@Test
+	public void shouldShowPagedSpittles() throws Exception {
+
+		List<Spittle> expectedSpittles = createSpittleList(50);
+		SpittleRepository mockRepository = mock(SpittleRepository.class);
+		when(mockRepository.findSpittles(Long.MAX_VALUE, 20)).thenReturn(expectedSpittles);
+		
+		SpittleController controller = new SpittleController(mockRepository);
+		
+		MockMvc mockMvc = standaloneSetup(controller)
+				.setSingleView(new InternalResourceView("/WEB-INF/views/spittles.jsp"))
+				.build();
+		
+		mockMvc.perform(get("/spittles"))
+			.andExpect(view().name("spittles"))
+			.andExpect(model().attributeExists("spittleList"))
+			.andExpect(model().attribute("spittleList", hasItems(expectedSpittles.toArray())));
+		
+	}
+	
+	 @Test
+	  public void testSpittle() throws Exception {
+	    Spittle expectedSpittle = new Spittle("Hello", new Date());
+	    SpittleRepository mockRepository = mock(SpittleRepository.class);
+	    when(mockRepository.findOne(12345)).thenReturn(expectedSpittle);
+	    
+	    SpittleController controller = new SpittleController(mockRepository);
+	    MockMvc mockMvc = standaloneSetup(controller).build();
+
+	    mockMvc.perform(get("/spittles/12345"))
+	      .andExpect(view().name("spittle"))
+	      .andExpect(model().attributeExists("spittle"))
+	      .andExpect(model().attribute("spittle", expectedSpittle));
+	  }
+	 
+	 @Test
+	 public void shouldShowRegistration() throws Exception {
+		 
+		 SpitterController controller = new SpitterController();
+		 MockMvc mockMvc = standaloneSetup(controller).build();
+		 
+		 mockMvc.perform(get("/spitter/register"))
+		 	.andExpect(view().name("registerForm"));
+	 }
+	
+
+	/**
+	 * 
+	 * @param count
+	 * @return
+	 * @description: 인스턴스 변수 만큼 반복문을 돌면서 데이터를 set하는 메소드
+	 */
+	private List<Spittle> createSpittleList(int count) {
+		List<Spittle> spittles = new ArrayList<Spittle>();
+		for (int i = 0; i < count; i++) {
+			spittles.add(new Spittle("Spittle " + i, new Date()));
+		}
+		return spittles;
+	}
+}
